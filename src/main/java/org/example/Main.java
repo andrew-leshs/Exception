d72c33422c3af79f022fa1a4e5942e81f6ebb65d
andrew-leshs <107064710+andrew-leshs@users.noreply.github.com>
2022-09-18 14:06:28 +0300

    XML

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,28 @@ package org.example;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static int[] prices = {50, 100, 250};
     public static String[] products = {"Bread", "Milk", "Cheese"};
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
 
         ClientLog clientLog = new ClientLog();
         Scanner scanner = new Scanner(System.in);
@@ -31,11 +42,105 @@ public class Main {
         int sum = 0;
 
         Basket basket = new Basket(prices, products);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File("shop.xml"));
+
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+
+        int count = 0;
+        List<String> loadList = new ArrayList<>();
+        List<String> saveList = new ArrayList<>();
+        List<String> logList = new ArrayList<>();
+
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            if (i == 1) {
+                for (int j = 0; j < nodeList.getLength(); j++) {
+                    Node book = nodeList.item(j);
+                    if (book.getNodeType() != Node.TEXT_NODE) {
+                        NodeList bookProps = book.getChildNodes();
+                        if (book.getNodeName().equals("load")) {
+                            for(int o = 0; o < bookProps.getLength(); o++) {
+                                Node bookProp = bookProps.item(o);
+                                if (bookProp.getNodeType() != Node.TEXT_NODE) {
+                                    loadList.add(bookProp.getChildNodes().item(0).getTextContent());
+                                }
+                            }
+                        }
+                        else if (book.getNodeName().equals("save")) {
+                            for(int o = 0; o < bookProps.getLength(); o++) {
+                                Node bookProp = bookProps.item(o);
+                                if (bookProp.getNodeType() != Node.TEXT_NODE) {
+                                    saveList.add(bookProp.getChildNodes().item(0).getTextContent());
+                                }
+                            }
+                        }
+                        else if (book.getNodeName().equals("log")) {
+                            for(int o = 0; o < bookProps.getLength(); o++) {
+                                Node bookProp = bookProps.item(o);
+                                if (bookProp.getNodeType() != Node.TEXT_NODE) {
+                                    logList.add(bookProp.getChildNodes().item(0).getTextContent());
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        String fileName = "";
+        for (int i = 0; i < loadList.size(); i++) {
+            if (i == 0) {
+                if (loadList.get(i).equals("true")) {
+
+                } else {
+                    break;
+                }
+            } else if (i == 1) {
+                fileName = loadList.get(i);
+            } else {
+                fileName = fileName + "." + loadList.get(i);
+            }
+        }
+
+        String saveFile = "";
+        for (int i = 0; i < saveList.size(); i++) {
+            if (i == 0) {
+                if (saveList.get(i).equals("true")) {
+
+                } else {
+                    break;
+                }
+            } else if (i == 1) {
+                saveFile = saveList.get(i);
+            } else {
+                saveFile = saveFile + "." + saveList.get(i);
+            }
+        }
+        String log = "";
+        for (int i = 0; i < logList.size(); i++) {
+            if (i == 0) {
+                if (logList.get(i).equals("true")) {
+
+                } else {
+                    break;
+                }
+            } else if (i == 1) {
+                log = logList.get(i);
+            } else {
+                log = log + "." + logList.get(i);
+            }
+        }
+
         JSONParser parser = new JSONParser();
         JSONObject jsonObject = new JSONObject();
-        if (new File("basket.json").exists()) {
+        if (new File(fileName).exists()) {
             try {
-                Object obj = parser.parse(new FileReader("basket.json"));
+                Object obj = parser.parse(new FileReader(fileName));
                 jsonObject = (JSONObject) obj;
 
                 JSONArray jsonArray = (JSONArray) jsonObject.get("array");
@@ -97,15 +202,18 @@ public class Main {
 
             if (productNumber < products.length) {
                 basket.addToCart(productNumber, productCount);
-                clientLog.log(productNumber + 1, productCount);
+                if (logList.get(0).equals("true")) {
+                    clientLog.log(productNumber + 1, productCount);
+                }
                 amount[productNumber] += productCount;
                 int sumProducts = productCount * prices[productNumber];
                 sum += sumProducts;
             }
         }
 
-        clientLog.exportAsCSV(new File("log.csv"));
-
+        if (logList.get(0).equals("true")) {
+            clientLog.exportAsCSV(new File(fileName));
+        }
 
         JSONObject rootObject = new JSONObject();
         System.out.println(basket.getBasketArray());
@@ -129,11 +237,17 @@ public class Main {
         rootObject.put("array", jsonArray);
         System.out.println();
 
-        try (FileWriter fileWriter = new FileWriter("basket.json")) {
-            fileWriter.write(rootObject.toJSONString());
-            fileWriter.flush();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        if (!saveList.get(0).equals("false")) {
+            if (saveList.get(2).equals("json")) {
+                try (FileWriter fileWriter = new FileWriter(saveFile)) {
+                    fileWriter.write(rootObject.toJSONString());
+                    fileWriter.flush();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                basket.saveTxt(new File(saveFile));
+            }
         }
 
         System.out.println("Your basket: ");
